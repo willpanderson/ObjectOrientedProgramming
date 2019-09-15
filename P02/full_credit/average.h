8f8c4671aec2423f14c1a59383ec77d72d0e24c3
wpa9071 <william.anderson@mavs.uta.edu>
2019-09-15 00:50:12 -0500

    Homework #4 full credit

--- P02/full_credit/average.h
+++ P02/full_credit/average.h
@@ -9,11 +9,12 @@
 class Average {
 public:
 	Average();
-	friend std::ostream& operator <<(std::ostream& ost, Average &average);
-	friend std::istream& operator >>(std::istream& ist, Average &average);
+	friend std::ostream& operator <<(std::ostream& ost, Average& average);
+	friend std::istream& operator >>(std::istream& ist, Average& average);
 	Average& operator+=(double value);
 private:
 	double _sum;
 	int _values;
+};
 
 #endif
