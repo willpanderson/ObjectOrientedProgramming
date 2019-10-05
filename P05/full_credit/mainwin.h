167f4399da609aade47bdbb2beaacfe9bcce9712
William Anderson <william.anderson@mavs.uta.edu>
2019-10-05 11:36:01 -0500

    "edited mainwin.h"

--- P05/full_credit/mainwin.h
+++ P05/full_credit/mainwin.h
@@ -10,11 +10,29 @@ class Mainwin : public Gtk::Window {
         Mainwin(Store& store);
         virtual ~Mainwin();
 
+    protected:
+        on_new_store_click();
+        on_add_sweet_click();
+        on_list_sweets_click();
+        on_place_order_click();
+        on_list_orders_click();
+        on_about_click();
+        on_quit_click();
+        ~reset_sensitivity();
+        
     private:
         Store *_store;
-
-    // Lots more Mainwin members are needed!
-    // (You ARE permitted to change the above as well if you like.)
+        Gtk::MenuItem *menuitem_add_sweet;
+        Gtk::MenuItem *menuitem_list_sweets;
+        Gtk::MenuItem *menuitem_place_orders;
+        Gtk::MenuItem *menuitem_list_orders;
+        Gtk::ToolButton *add_sweet_button;
+        Gtk::ToolButton *list_sweets_button;
+        Gtk::ToolButton *place_order_button;
+        Gtk::ToolButton *list_orders_button;
+        Gtk::Label *data;
+        Gtk::Label *msg;
+    
 };
 #endif 
 
