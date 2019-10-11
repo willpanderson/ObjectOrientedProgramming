baa6eaeda970d8f5082a0981cb22c4996b505978
William Anderson <wpa9071@Williams-MacBook-Pro.local>
2019-10-10 21:58:19 -0500

    Edited mainwin.cpp

--- P07/full_credit/mainwin.cpp
+++ P07/full_credit/mainwin.cpp
@@ -1,13 +1,46 @@
 #include "mainwin.h"
+#include <cmath>
 
-Mainwin::Mainwin() : button{new Gtk::Button{"Hello, world!"}} {
-  add(*button);
-  button->signal_clicked().connect([this] {this->on_button_click();});
-  button->show();
+
+Mainwin::Mainwin() : 
+{
+	Gtk::VBox *vbox = Gtk::manage(new Gtk::Vbox);
+	add (*vbox);
+	set_title("Math Functions");
+
+	vbox->pack_start(*comboboxtext);
+	comboboxtext->append("Square");
+	comboboxtext->append("Square Root");
+	comboboxtext->append("Natural Log");
+	comboboxtext->set_active(1);
+	comboboxtext->signal_changed().connect([this] {this->on_comboboxtext_activate();});
+	
+vbox->show_all();
 }
 
 Mainwin::~Mainwin() { }
 
-void Mainwin::on_button_click() {
-    Gtk::MessageDialog{"Ouch!"}.run();
+void Mainwin::on_entry_activate()
+{
+	std::stringstream text;
+	text << this entry->get_text();
+	text >> *s2;
+}
+
+Void Mainwin::oncomboboxtext_activate()
+{
+	if (comboboxtext->get_row_number() == 1)
+	{
+		label->set_text(std::to_string(s2*s2);
+	}
+
+	else if (comboboxtext->get_row_number() == 2)
+	{
+		label->set_text(std::to_string(sqrt(s2)));
+	}
+	
+	else if (comboboxtext->get_row_number() == 3)
+	{
+		label->set_text(std::to_string(log10(s2)));
+	}
 }
