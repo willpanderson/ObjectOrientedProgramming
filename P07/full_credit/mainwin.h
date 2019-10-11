dd97ed30f0ab63ff16dcfcbc2be59898ab9cb5e6
William Anderson <wpa9071@Williams-MacBook-Pro.local>
2019-10-11 11:47:58 -0500

    final

--- P07/full_credit/mainwin.h
+++ P07/full_credit/mainwin.h
@@ -4,11 +4,12 @@ class Mainwin : public Gtk::Window {
   public:
     Mainwin();
     virtual ~Mainwin();
+
   protected:
     void on_scale_value_changed();
     void oncomboboxtext_activate();
   private:
-    // These are the widgets demonstrated in order
+    
     Gtk::Scale* scale;
     Gtk::ComboBoxText* comboboxtext;
     Gtk::Label* label;
