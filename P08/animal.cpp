3e86143ba21aba147b8e6c8489448e084a6ce674
wpa9071 <47925572+wpa9071@users.noreply.github.com>
2019-10-30 23:39:56 -0500

    Update animal.cpp

---
+++ P08/animal.cpp
@@ -0,0 +1,31 @@
+void Animal(std::string name, Gender gender, Gender age,int age)
+{
+}
+
+void ~Animal()
+{
+}
+
+std::string family()
+{
+}
+
+std::string breed()
+{
+}
+
+std::string name()
+{
+}
+
+Gender gender()
+{
+}
+
+int age()
+{
+}
+
+std::string to_string()
+{
+}
