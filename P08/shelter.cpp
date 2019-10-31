4a48cf27c625cff5a07d19f7e1592695037afc91
wpa9071 <47925572+wpa9071@users.noreply.github.com>
2019-10-30 23:37:52 -0500

    Update shelter.cpp

---
+++ P08/shelter.cpp
@@ -0,0 +1,25 @@
+#include "shelter.cpp"
+
+
+void Shelter(std::string name)
+{
+}
+
+void ~Animal()
+{
+}
+
+std::string name()
+{
+}
+void add_animal (Animal& animal)
+{
+}
+
+int num_animals()
+{
+}
+
+Animal& animal(int index)
+{
+}
