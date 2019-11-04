c7edd69486e65ca97e3e3f230919c10b62159126
wpa9071 <wpa9071@users.noreply.github.com>
2019-11-03 22:25:01 -0600

    Update shelter.cpp

--- P08/shelter.cpp
+++ P08/shelter.cpp
@@ -5,22 +5,22 @@ void Shelter(std::string name)
 {
 }
 
-std::string Shelter::name() 
+std::string Shelter::name()
 {
- //return _name;
+ return _name;
 }
 
 void Shelter::add_animal (Animal& animal)
 {
- //return _avalible.push_back(animal);
+ return _avalible.push_back(animal);
 }
 
 int Shelter::num_animals()
 {
-//return _avalible.size();
+ return _avalible.size();
 }
 
 Animal& Shelter::animal(int index)
 {
- //return _avalible[index];
+ return _avalible[index];
 }
