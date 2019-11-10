742a2b159400bf6d2d74175760d3afb2ac34db76
wpa9071 <47925572+wpa9071@users.noreply.github.com>
2019-11-10 15:39:20 -0600

    push

--- P08/client.cpp
+++ P08/client.cpp
@@ -2,8 +2,3 @@
 
 Client::Client(std::string name, Gender gender, int age)
     : _name{name}, _phone{phone}, _email{email} { }
-
-//std::ostream& operator<<(std::ostream& ost, const Client& client) {
-  //  ost << to_string(client);
-    //return ost;
-//}
