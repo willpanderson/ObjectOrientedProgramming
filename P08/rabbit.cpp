3159c1c196b3f31243b4c8c136fd58b53e085905
wpa9071 <47925572+wpa9071@users.noreply.github.com>
2019-11-10 16:02:37 -0600

    push

--- P08/rabbit.cpp
+++ P08/rabbit.cpp
@@ -12,18 +12,18 @@ std::string Rabbit::breed() const {return ::to_string(_breed);}
 // Convert breed to string and stream - use a std::map for other derived classes!
 std::string to_string(const rabbit_breed& breed) {
     switch(breed) {
-        case  rabbit_breed::SHORTHAIR:  return "Shorthair";
-        case  rabbit_breed::BOMBAY:     return "Giant";
-        case  rabbit_breed::PERSIAN:    return "Fuzzy";
-        case  rabbit_breed::BENGAL:     return "Gotland";
-        case  rabbit_breed::SPHYNX:     return "Harlequin";
-        case  rabbit_breed::BURMESE:    return "Palomino";
-        case  rabbit_breed::BOBTAIL:    return "Sable";
-        case  rabbit_breed::MAU:        return "Angora";
-        case  rabbit_breed::LYKOI:      return "Dwarf";
-        case  rabbit_breed::KORAT:      return "Wooly";
-        case  rabbit_breed::MANX:       return "Chinchilla";
-        case  rabbit_breed::SIAMESE:    return "Lionhead";
+        case  rabbit_breed::HARE:       return "Hare";
+        case  rabbit_breed::GIANT:      return "Giant";
+        case  rabbit_breed::FUZZY:      return "Fuzzy";
+        case  rabbit_breed::GOTLAND:    return "Gotland";
+        case  rabbit_breed::HARLEQUIN:  return "Harlequin";
+        case  rabbit_breed::PALOMINO:   return "Palomino";
+        case  rabbit_breed::SABLE:      return "Sable";
+        case  rabbit_breed::ANGORA:     return "Angora";
+        case  rabbit_breed::DWARF:      return "Dwarf";
+        case  rabbit_breed::WOOLY:      return "Wooly";
+        case  rabbit_breed::CHINCHILLA: return "Chinchilla";
+        case  rabbit_breed::LIONHEAD:   return "Lionhead";
         default:                        return "UNKNOWN";
     }
 }
