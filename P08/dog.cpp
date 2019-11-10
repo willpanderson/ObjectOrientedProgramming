1044ea90ed8be07ca7e5d83481b327bb72ecd60f
wpa9071 <47925572+wpa9071@users.noreply.github.com>
2019-11-10 14:05:09 -0600

    push

--- P08/dog.cpp
+++ P08/dog.cpp
@@ -13,23 +13,6 @@ std::string Dog::family() const {return "dog";}
 std::string Dog::breed() const {return ::to_string(_breed);}
 
 // Convert breed to string and stream - use a std::map for other derived classes!
-std::string to_string(const Dog_breed& breed)
-{
-    switch(breed) {
-        case  Dog_breed::MIX:        return "Mix";
-        case  Dog_breed::LABRADOR:   return "Labrador";
-        case  Dog_breed::RETRIEVER:  return "Retriever";
-        case  Dog_breed::SHEPHERD:   return "Shepherd";
-        case  Dog_breed::BULLDOG:    return "Bulldog";
-        case  Dog_breed::BEAGLE:     return "Beagle";
-        case  Dog_breed::POODLE:     return "Poodle";
-        case  Dog_breed::ROTTWEILER: return "Rottweiler";
-        case  Dog_breed::POINTER:    return "Pointer";
-        case  Dog_breed::TERRIER:    return "Terrier";
-        case  Dog_breed::BOXER:      return "Boxer";
-        case  Dog_breed::DACHSHUND:  return "Dachshund";
-        default:                     return "UNKNOWN";
-    }
     int dogselect;
     std::cout << "what kind of dog do you want?" <<std::endl;
     std::cin >> dogselect;
