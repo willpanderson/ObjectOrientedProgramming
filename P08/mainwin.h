c32a050ee0ad2df5e6a50d0ad14da66b83144edd
wpa9071 <47925572+wpa9071@users.noreply.github.com>
2019-11-10 16:06:24 -0600

    push

--- P08/mainwin.h
+++ P08/mainwin.h
@@ -16,12 +16,11 @@ class Mainwin : public Gtk::Window {
         void on_quit_click();              // Exit the application
         void on_new_animal_click();        // Create a new animal
         void on_list_animals_click();      // List all animals
-
+        void on_new_client_click();        // Creaate a new client
         void status(std::string s);        // Set the status message
     private:
         Shelter *shelter;                  // Current animal shelter
         Gtk::Label *data;                  // Display of sticks on game board
         Gtk::Label *msg;                   // Status message display
 };
-#endif 
-
+#endif
