39a70d02c3646e21d149dd8c1ee2d0c0034f0d47
wpa9071 <47925572+wpa9071@users.noreply.github.com>
2019-11-12 21:24:45 -0600

    edited files

--- P13/primes.cpp
+++ P13/primes.cpp
@@ -1,7 +1,6 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
-#include <mutex>
 
 // To measure performance, use
 // $ make timep
@@ -12,7 +11,7 @@
 class Prime_numbers {
   public:
     Prime_numbers(int num_threads = 1): NUM_THREADS{num_threads} { }
-    std::mutex m;
+
     // Returns true if "number" is a prime number
     bool is_prime (int number) {
       if (number < 2) return false;
@@ -26,22 +25,14 @@ class Prime_numbers {
     //     adding each prime found to the shared vector "primes".
     // NOTE: You'll likely need to break out the for loop as a separate
     //     method, which will be the code executed by each thread.
-    //     The find_prime method will just create and manage the threads.
-    void find_primes_thread(int lower, int upper) {
-        for (int i=lower; i<=upper; ++i) {
-            if (is_prime(i)) {
-              m.unlock();  primes.push_back(i); m.lock();
-            }
-        }
-      }
-
-  void find_primes(int lower, int upper) {
+    //     The find_prime method will just create and managed the threads.
+    void find_primes(int lower, int upper) {
         for (int i=lower; i<=upper; ++i) {
             if (is_prime(i)) {
                 primes.push_back(i);
             }
         }
-    }
+    }    
 
     typedef std::vector<int> Primes;
 
@@ -65,13 +56,14 @@ int main(int argc, char* argv[]) {
     Prime_numbers prime_numbers(threads);
 
     // Determine maximum integer to search
-    int max_int = 12000000;
+    int max_int = 10000000;
     if(argc > 2) max_int = atoi(argv[2]);
 
     // Search and identify all primes between 2 and max_int
     prime_numbers.find_primes(2, max_int);
 
-    // Print all primes that were found
+    // Print all primes that were found  
     for (int p : prime_numbers) std::cout << p << '\n';
     std::cout << std::endl;
 }
+
