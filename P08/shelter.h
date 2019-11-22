ba7182bbefbfdfaa02f14243432fe1f912f69ebf
wpa9071 <47925572+wpa9071@users.noreply.github.com>
2019-11-22 16:56:55 -0600

    e

--- P08/shelter.h
+++ P08/shelter.h
@@ -16,12 +16,11 @@ class Shelter {
     int num_clients();
     Client& client(int index);
     void adopt(Client& client, Animal& animal);
-    std::string filename();
     void set_filename(std::string filename);
     //void save(std::ostream& ost);
     //void load(std::istream& ist);
   protected:
-   std::string filename;
+   std::string filename();
 
   private:
     std::string _name;
