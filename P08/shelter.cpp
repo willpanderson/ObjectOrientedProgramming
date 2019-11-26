424d3556e1e83069e7998f4938eae2e8c1c455c5
wpa9071 <47925572+wpa9071@users.noreply.github.com>
2019-11-25 22:58:48 -0600

    e

--- P08/shelter.cpp
+++ P08/shelter.cpp
@@ -16,10 +16,24 @@ void Shelter::add_client(Client& client) {
 int Shelter::num_clients() {return _clients.size();}
 Client& Shelter::client(int index) {return _clients[index];}
 
-void Shelter::adopt(Client& client, Animal& animal) {
-    client.adopt(animal);
-    auto index = std::find(_available.begin(), _available.end(), &animal);
-    if (index != _available.end()) _available.erase(index);
+void Shelter::adopt(Client& client,Animal& animal) {
+ int index = -1;
+/*animals might have the same name,breed,gender,family, and age so
+//we will have to check each aspect to ensure that the right animal is removed */
+ for (int i = 0; i < _available.size(); i++)
+ {
+  if (_available[i]->name() == animal.name()){
+    if (_available[i]->family() == animal.family()){
+      if (_available[i]->breed() == animal.breed()){
+        if (_available[i]->gender() == animal.gender()){
+            index = i;
+	}
+     }
+   }
+  }
+ }
+ client.adopt(animal);
+ this->_available.erase(_available.begin()+index);
 }
 
 std::string Shelter::get_filename() {return filename;}
@@ -27,7 +41,7 @@ void Shelter::set_filename(std::string filename) {this->filename = filename;}
 
 void Shelter::save(std::ostream& ost)
 {
-  ost << _name << "\n";
+  ost << _name << std::endl;
 
  for (auto a : _available)
  {
